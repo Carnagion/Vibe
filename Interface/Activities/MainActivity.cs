edbc8821e16b4eb5697cbaf4d956bf628c1150a0
Carnagion <carnagion.games@gmail.com>
2022-06-03 17:01:43 +0200

    Overhaul main activity

--- Interface/Activities/MainActivity.cs
+++ Interface/Activities/MainActivity.cs
@@ -1,5 +1,4 @@
 using System;
-using System.Linq;
 
 using Android.App;
 using Android.Content;
@@ -7,9 +6,8 @@ using Android.OS;
 using Android.Support.Design.Widget;
 using Android.Support.V4.App;
 using Android.Support.V4.View;
-using Android.Support.V4.Widget;
 using Android.Support.V7.App;
-using Android.Views;
+using Android.Support.V7.Widget;
 
 using Java.Lang;
 
@@ -20,7 +18,6 @@ using Vibe.Utility.Extensions;
 
 using Fragment = Android.Support.V4.App.Fragment;
 using FragmentManager = Android.Support.V4.App.FragmentManager;
-using Toolbar = Android.Support.V7.Widget.Toolbar;
 using String = Java.Lang.String;
 
 namespace Vibe.Interface.Activities
@@ -28,64 +25,32 @@ namespace Vibe.Interface.Activities
     [Activity(Label = "@string/app_name", Theme = "@style/Theme.Vibe", MainLauncher = false, NoHistory = false)]
     internal sealed class MainActivity : AppCompatActivity
     {
-        private DrawerLayout drawerLayout = null!;
-
-        private NavigationView navigationView = null!;
-
         private NowPlayingFragment nowPlayingFragment = null!;
         
-        public override bool OnOptionsItemSelected(IMenuItem item)
-        {
-            if (item.ItemId is not 16908332)
-            {
-                return base.OnOptionsItemSelected(item);
-            }
-            if (this.drawerLayout.IsDrawerOpen(this.navigationView))
-            {
-                this.drawerLayout.CloseDrawer(this.navigationView);
-            }
-            else
-            {
-                this.drawerLayout.OpenDrawer(this.navigationView);
-            }
-            return true;
-        }
-
         protected override void OnCreate(Bundle? savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             
-            this.SetContentView(Resource.Layout.activity_main);
-
-            ViewPager pager = this.FindViewById<ViewPager>(Resource.Id.activity_main_pager)!;
-            pager.Adapter = new MainViewPagerAdapter(this.SupportFragmentManager);
-            this.FindViewById<TabLayout>(Resource.Id.activity_main_tabs)!.SetupWithViewPager(pager);
-            
-            this.SetSupportActionBar(this.FindViewById<Toolbar>(Resource.Id.activity_main_toolbar)!);
-            this.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
-            this.SupportActionBar.SetDisplayShowTitleEnabled(false);
-            this.SupportActionBar.SetHomeButtonEnabled(true);
-            this.SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.icon_main_menu);
-            this.drawerLayout = this.FindViewById<DrawerLayout>(Resource.Id.activity_main_drawer)!;
-            this.navigationView = this.FindViewById<NavigationView>(Resource.Id.activity_main_navigator)!;
-            this.drawerLayout.CloseDrawer(this.navigationView);
-            this.navigationView.NavigationItemSelected += this.OnNavigationViewNavigationItemSelected;
-            
+            // If savedInstanceState is null, it indicates that the activity has been created for the first time, and not due to changing orientations or other reasons
             if (savedInstanceState is null)
             {
                 Playback.Setup();
             }
+            
+            this.SetContentView(Resource.Layout.activity_main);
+            
+            this.SetupActionBar();
+            this.SetupViewPager();
+            this.SetupNowPlayingService();
 
-            Intent service = new(this, typeof(NowPlayingService));
-            service.SetAction(ForegroundService.start);
-            this.StartForegroundService(service);
-
-            this.nowPlayingFragment = (NowPlayingFragment)this.SupportFragmentManager.FindFragmentById(Resource.Id.activity_main_fragment_nowplaying);
+            this.nowPlayingFragment = (NowPlayingFragment)this.SupportFragmentManager.FindFragmentById(Resource.Id.activity_main_fragment_now);
         }
 
         protected override void OnResume()
         {
             base.OnResume();
+            
+            // Show now playing upon resume if necessary
             if (this.nowPlayingFragment.IsHidden && Playback.NowPlaying is not null && Playback.PlayingState is not (Playback.MediaPlayerState.Completed or Playback.MediaPlayerState.Stopped or Playback.MediaPlayerState.End))
             {
                 this.nowPlayingFragment.Show(true);
@@ -95,29 +60,43 @@ namespace Vibe.Interface.Activities
         protected override void OnDestroy()
         {
             base.OnDestroy();
-            
-            this.navigationView.NavigationItemSelected -= this.OnNavigationViewNavigationItemSelected;
-            
+
+            // If not finishing, it indicates that the activity will be recreated again due to changing orientations or other reasons
             if (!this.IsFinishing)
             {
                 return;
             }
+            
             Playback.End();
-            Library.SavePersistentData();
         }
 
-        private void OnNavigationViewNavigationItemSelected(object source, NavigationView.NavigationItemSelectedEventArgs eventArgs)
+        // Setup the toolbar
+        private void SetupActionBar()
         {
-            switch (eventArgs.MenuItem.ItemId)
-            {
-                case Resource.Id.menu_main_navigator_settings:
-                    break;
-                case Resource.Id.menu_main_navigator_about:
-                    break;
-            }
-            this.drawerLayout.CloseDrawer(this.navigationView);
+            this.SetSupportActionBar(this.FindViewById<Toolbar>(Resource.Id.activity_main_toolbar));
+            /*this.SupportActionBar.SetDisplayHomeAsUpEnabled(true);
+            this.SupportActionBar.SetDisplayShowTitleEnabled(false);
+            this.SupportActionBar.SetHomeButtonEnabled(true);
+            this.SupportActionBar.SetHomeAsUpIndicator(Resource.Drawable.menu);*/
         }
 
+        // Setup the view pager to work with the tabs
+        private void SetupViewPager()
+        {
+            ViewPager viewPager = this.FindViewById<ViewPager>(Resource.Id.activity_main_viewpager)!;
+            viewPager.Adapter = new MainViewPagerAdapter(this.SupportFragmentManager);
+            this.FindViewById<TabLayout>(Resource.Id.activity_main_tabs)!.SetupWithViewPager(viewPager);
+        }
+
+        // Setup the now playing notification service
+        private void SetupNowPlayingService()
+        {
+            Intent service = new(this, typeof(NowPlayingService));
+            service.SetAction(ForegroundService.start);
+            this.StartForegroundService(service);
+        }
+        
+        // Adapter class for tabs
         private sealed class MainViewPagerAdapter : FragmentStatePagerAdapter
         {
             public MainViewPagerAdapter(FragmentManager fragmentManager) : base(fragmentManager)
@@ -128,10 +107,11 @@ namespace Vibe.Interface.Activities
             {
                 get
                 {
-                    return 5;
+                    return 3;
                 }
             }
 
+            // Return the corresponding fragment for each tab
             public override Fragment GetItem(int position)
             {
                 return position switch
@@ -139,23 +119,18 @@ namespace Vibe.Interface.Activities
                     0 => new ArtistListFragment(Library.Artists),
                     1 => new AlbumListFragment(Library.Albums),
                     2 => new TrackListFragment(Library.Tracks),
-                    3 => new PlaylistListFragment(from playlist in Library.Playlists
-                                                  orderby playlist.Title
-                                                  select playlist),
-                    4 => new TrackListFragment(Enumerable.Empty<Track>()),
                     _ => throw new ArgumentOutOfRangeException(nameof(position)),
                 };
             }
 
+            // Must be overriden for tab layout titles to display properly. Xamarin moment
             public override ICharSequence GetPageTitleFormatted(int position)
             {
                 return position switch
                 {
-                    0 => new String(Application.Context.GetString(Resource.String.tab_artists)),
-                    1 => new(Application.Context.GetString(Resource.String.tab_albums)),
-                    2 => new(Application.Context.GetString(Resource.String.tab_tracks)),
-                    3 => new(Application.Context.GetString(Resource.String.tab_playlists)),
-                    4 => new(Application.Context.GetString(Resource.String.tab_compilations)),
+                    0 => new String(Application.Context.GetString(Resource.String.activity_main_tab_artists)),
+                    1 => new(Application.Context.GetString(Resource.String.activity_main_tab_albums)),
+                    2 => new(Application.Context.GetString(Resource.String.activity_main_tab_tracks)),
                     _ => throw new ArgumentOutOfRangeException(nameof(position)),
                 };
             }
